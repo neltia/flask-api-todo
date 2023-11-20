e1fc93156b973cca64507d4db8ebe2571dfee634
neltia <dsz08082@naver.com>
2023-11-21 00:31:22 +0900

    fix: es mapping 설정 변경
    priority: Text->Keyword, task: nori analyzer add

--- 05_todo_api_es/config/es_config.py
+++ 05_todo_api_es/config/es_config.py
@@ -1,11 +1,20 @@
-from elasticsearch_dsl import connections
 from dotenv import load_dotenv
 import os
+from elasticsearch_dsl import connections
+from elasticsearch_dsl import Index
+from app.models.dao import Todo
 
 
+# es 설정 초기화
 def es_init():
     load_dotenv(verbose=True)
 
+    es_client()
+    index_mapping()
+
+
+# 엘라스틱서치 클라이언트 세션 생성
+def es_client():
     # es host info
     es_api_key = os.getenv("ES_API_KEY")
     es_host = os.getenv("ES_HOST")
@@ -21,3 +30,9 @@ def es_init():
         ca_certs=ca_file_path,
         api_key=es_api_key
     )
+
+
+# nori plugin 적용을 위한 매핑 설정
+def index_mapping():
+    Todo.init()
+    return
